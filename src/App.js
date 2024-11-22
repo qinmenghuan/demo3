c5ee46194ca01184b1eac6caf40caaedfa12a827
qinmenghuan <1250397216@qq.com>
2024-11-22 21:06:21 +0800

    feat: add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Add from "./components/Add";
 import Greetings from "./components/Greetings";
 import { Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
 
+ 
 function App() {
   return (
     <Fragment>
       <Greetings/>
       <Add/>
+      <h1 className="text-orange-400 text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </Fragment>
   );
 }
